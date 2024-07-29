6e581bb7eef5749ce94a437b0810801570bb8671
Maryam Asaria <maryam.asaria@gmail.com>
2024-07-29 15:27:26 +0100

    Update forms.py
    Created forms for even and custom choices

--- paypart/logic/forms.py
+++ paypart/logic/forms.py
@@ -4,9 +4,15 @@ from django import forms
 class SplitForm(forms.Form):
     num_people = forms.IntegerField(min_value=1, label='Number of people splitting payment')
 
+
 class SplitChoiceForm(forms.Form):
     SPLIT_CHOICES = [
         ('even', 'Even Split'),
         ('custom', 'Custom Split'),
     ]
     split_type = forms.ChoiceField(choices=SPLIT_CHOICES, widget=forms.RadioSelect)
+
+
+class UsernameForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=100)
+    amount = forms.DecimalField(label='Amount', required=False, min_value=0)
