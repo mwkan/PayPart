5dbd0e048d6b1ca473c915458d1744a1b51ef0fe
Mun Wei Kan <munny.kan@hotmail.co.uk>
2024-07-29 11:21:23 +0100

    create database, add links, and draft home page

--- paypart/logic/models.py
+++ paypart/logic/models.py
@@ -5,7 +5,7 @@ class Person(models.Model):
     name = models.CharField(max_length=200)
     # bank = options
     payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    #modify depending on API input
+    # modify depending on API input
 
     def __str__(self):
         return self.name
